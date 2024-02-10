45b4ec80c1af90c4f369ed3762ef86d994bff273
iiRyan <92063117+iiRyan@users.noreply.github.com>
2024-02-10 21:54:19 +0300

    Create CourseDao

---
+++ src/main/java/com/rayan/onlinecourses/dao/CourseDao.java
@@ -0,0 +1,17 @@
+package com.rayan.onlinecourses.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.*;
+
+import com.rayan.onlinecourses.entity.Course;
+
+public interface CourseDao extends JpaRepository<Course, Long> {
+
+    List<Course> findCourseByCourseNameContains(String keyword);
+
+    @Query(value = "SELECT * from courses AS c WHERE c.course_id in (SELECT e.course_id FROM enrolled_in AS e WHERE e.student_id =:studentId)", nativeQuery = true)
+    List<Course> getCoursesByStudentId(@Param("studentId") Long studentId);
+}
